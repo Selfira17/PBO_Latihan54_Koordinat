c84ad789368c2de42670ca89ffa780172cab9815
Selfira <81574694+Selfira17@users.noreply.github.com>
2021-04-01 21:11:36 +0700

    Add files via upload
    Latihan PBO

---
+++ PBO_Latihan54_Koordinat.java
@@ -0,0 +1,37 @@
+/*
+Nama    : Selfira Anyuniarwati
+Kelas   : PBO (FS112B)
+NIM     : 2031332802
+Deskripsi : Latihan 54-Koordinat
+ */
+package pbo_latihan54_koordinat;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBO_Latihan54_Koordinat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    String namawarna = " Jingga ";
+    int koordinat = 10;
+    int kordinat = 4;
+    
+    System.out.println("Warna Koordinat   :"+namawarna);
+    System.out.println("Koordinat Sumbu x :"+koordinat+","+"y :"+kordinat);
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
